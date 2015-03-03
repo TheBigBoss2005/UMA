4ec464696355259202dd8bf8543193c38eddec07
19861115 <ipewlm@hotmail.co.jp>
2015-03-03 21:50:57 +0900

    写真選定ページのボタン押下時の挙動を追加

--- spec/requests/select_pages_spec.rb
+++ spec/requests/select_pages_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe 'PicturesSelectPages' do
   describe 'GET /pictures/select' do
+    title = '写真選定'
+
     before do
       10.times { FactoryGirl.create(:picture) }
     end
 
     describe 'ページタイトル' do
-      title = '写真選定'
+      # title = '写真選定'
       it "は#{title}" do
         visit pictures_select_path
         expect(page).to have_title(title)
@@ -17,7 +19,9 @@ describe 'PicturesSelectPages' do
     describe '写真が未登録の時' do
       describe 'メイン部分' do
         it '写真が未登録である旨のメッセージが表示される' do
-          #
+          Picture.delete_all
+          visit pictures_select_path
+          expect(page).to have_content('写真が存在しません')
         end
       end
     end
@@ -28,6 +32,22 @@ describe 'PicturesSelectPages' do
           visit pictures_select_path
           expect(page).to have_selector('button#left img')
           expect(page).to have_selector('button#right img')
+        end
+      end
+
+      describe '左側ボタン押下時' do
+        it '写真選定画面が更新される' do
+          visit pictures_select_path
+          click_button 'left'
+          expect(page).to have_title(title)
+        end
+      end
+
+      describe '右側ボタン押下時' do
+        it '写真選定画面が更新される' do
+          visit pictures_select_path
+          click_button 'right'
+          expect(page).to have_title(title)
         end
       end
     end
