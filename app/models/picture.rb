e8754ac1f80aa3b5d6268fb7081dde04715c7925
Suguru Akiho <suguru.akiho@gmail.com>
2015-03-17 07:47:02 +0900

    レビュー指摘の修正

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -15,7 +15,7 @@ class Picture < ActiveRecord::Base
 
   def self.iine2_total(num)
     # 0除算を避けるためにtotal_countに+1して除算している
-    limit(num).order('iine_count * 2 + 1 / (total_count + 1) desc, iine_count desc')
+    limit(num).order('(iine_count * 2 + 1) / (total_count + 1) desc, iine_count desc')
   end
 
   private_class_method :random, :iine2_total
