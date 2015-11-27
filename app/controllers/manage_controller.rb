6bf406e9daafeacc0b8e37f9f38e653ff8b9129c
Suguru Akiho <suguru.akiho@gmail.com>
2015-11-27 22:39:17 +0900

    ページングしてもランキングが正しく表示されるように修正

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -1,10 +1,18 @@
 class ManageController < ApplicationController
   def list
     only_choosed = params[:only_choosed] == 'true'
-    @pictures = only_choosed ? Picture.choosed : Picture.ranking(30, params[:page])
+    page_size = 3
+    @ranking_offset = calc_ranking_offset(page_size)
+    @pictures = only_choosed ? Picture.choosed : Picture.ranking(page_size, params[:page])
     @total_choosed = Picture.total_choosed
     @years_choosed = (2005..2015).each_with_object({}) do |year, res|
       res[year.to_s] = Picture.total_choosed(year)
     end
+  end
+
+  private
+
+  def calc_ranking_offset(page_size)
+    page_size * ((params[:page] || 1).to_i - 1)
   end
 end
